62c85a4d2187ccedc90763582304496846c5ba1f
Alvaroooooooo <90764257+Alvaroooooooo@users.noreply.github.com>
2022-01-13 15:00:17 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,11 @@ from LogIn import *
 from CreateExcel import *
 
 path=str(pathlib.Path(__file__).parent.absolute())
-driver = webdriver.Chrome(path+'/chromedriver')
+
+try:
+    driver = webdriver.Chrome(path+'/chromedriver')
+except:
+    driver = webdriver.Chrome(path+'/chromedriver.exe')
 
 logIn(driver)
 createExcel(driver)
